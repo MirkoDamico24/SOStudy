19dfcdd2f77dc823050491f08c0de0933a955930
MirkoDamico24 <mirko.damico04@gmail.com>
2026-08-11 17:30:40 +0200

    use case 'create test' partially implemented

--- src/main/java/it/uniroma2/dicii/ispw/sostudy/dao/virtualClass/VirtualClassDemoDAO.java
+++ src/main/java/it/uniroma2/dicii/ispw/sostudy/dao/virtualClass/VirtualClassDemoDAO.java
@@ -4,7 +4,7 @@ import it.uniroma2.dicii.ispw.sostudy.model.Professor;
 import it.uniroma2.dicii.ispw.sostudy.model.VirtualClass;
 
 public class VirtualClassDemoDAO implements VirtualClassDAO {
-    @Overridet
+    @Override
     public VirtualClass getVirtualClassByName(String name){
         return null;
     }
